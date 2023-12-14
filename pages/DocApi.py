db6db8733b224ca5b916616ff0f78c017606e17a
Edison MASSENA <massenaedison0@outlook.fr>
2023-12-14 16:55:11 +0100

    maj

--- pages/DocApi.py
+++ pages/DocApi.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-components.iframe("http://localhost:8000/docs", height=800, scrolling=True, width=1000)
+components.iframe("http://localhost:8000/docs", height=800, scrolling=True, width=800)
